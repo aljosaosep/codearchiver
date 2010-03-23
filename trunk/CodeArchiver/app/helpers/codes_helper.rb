60f7f18471b13d079467171f920c6870be995666
LaMM3R <LaMM3R@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-03-23 17:12:31 +0000

    popravljeno se za program language

--- trunk/CodeArchiver/app/helpers/codes_helper.rb
+++ trunk/CodeArchiver/app/helpers/codes_helper.rb
@@ -5,4 +5,13 @@ module CodesHelper
   def authorName(thisCode)
     return User.find(thisCode.user_id).username
   end
+  
+  # Returns programLanguage as string
+  # Input: code class
+  # Output: programLanguage name
+  def programLanguageName(thisCode)
+    return ProgramLanguage.find(thisCode.program_language_id).language_name
+  end
+  
+  
 end
