ac8fb5ca476601e5730f0665d9b651abac974794
LaMM3R <LaMM3R@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-03-23 15:31:10 +0000

    Posodobljeno da izpisuje avtorja koda in dodana "funkcija" v code_helper

--- CodeArchiver/app/models/code.rb
+++ CodeArchiver/app/models/code.rb
@@ -1,6 +1,18 @@
 class Code < ActiveRecord::Base
+  validates_presence_of :code_body
+  validates_presence_of :title
+  validates_length_of :title, :maximum=>40
+  validates_presence_of :description
+  validates_presence_of :shortdescription
+  validates_length_of :shortdescription, :maximum=>50, :message=>": Maximum 50 character allowed"
+  
   belongs_to :user
   belongs_to :program_language
   belongs_to :category
   belongs_to :type
+  
+  # Added by Edo, 23.3.2010
+  has_many :grades
+  has_one :code_replies
+  has_many :comments
 end
