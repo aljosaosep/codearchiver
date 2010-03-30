2f87e0456eb8b2913a04e44fe8a2cd790d768299
LaMM3R <LaMM3R@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-03-30 10:55:26 +0000

    Ob ustvarjanju novega userja se ustvari tudi nov profil z ustreznim user-id-jem

--- CodeArchiver/app/controllers/users_controller.rb
+++ CodeArchiver/app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
     @user = User.new(params[:user])
     if @user.save
       session[:user_id] = @user.id
+      Profile.create(:user_id => @user.id)
       flash[:notice] = "Thank you for signing up! You are now logged in."
       redirect_to new_session_path
     else
