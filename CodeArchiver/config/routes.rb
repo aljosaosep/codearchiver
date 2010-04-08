6ca990ab4ab865eec350774bbb57e8d56431e622
LaMM3R <LaMM3R@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-04-08 11:50:51 +0000

    ok now signup works :D

--- CodeArchiver/config/routes.rb
+++ CodeArchiver/config/routes.rb
@@ -9,7 +9,9 @@ ActionController::Routing::Routes.draw do |map|
   map.users 'users/:username', :controller => 'users', :action => 'show'
  # map.users.connect ':controller/:action/:username'
   
-  map.signup 'signup', :controller => 'users', :action => 'new'
+  
+  #new -> create
+  map.signup 'signup', :controller => 'users', :action => 'create'
   map.logout 'logout', :controller => 'sessions', :action => 'destroy'
   map.login 'login', :controller => 'sessions', :action => 'new'
   map.resources :sessions
