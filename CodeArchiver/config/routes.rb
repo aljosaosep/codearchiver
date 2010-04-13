b3e1fb353e11ec958134e0b4e08e343466c99078
jernej.halozan <jernej.halozan@gmail.com>
2010-04-13 18:35:10 +0000

    uredil routes

--- CodeArchiver/config/routes.rb
+++ CodeArchiver/config/routes.rb
@@ -1,28 +1,30 @@
 ActionController::Routing::Routes.draw do |map|
   
   map.root :controller => "main"
- 
-  #12.04
-  #map.commentform 'code_list/:id', :controller => 'code_list', :action => 'new_comment'  
-  #map.commentform 'code_list/:id', :controller => 'code_list', :action => 'show'  
+  
+  map.myprofile 'profile', 
+    :controller => 'profiles',
+    :action => 'show'
+  
+  map.userji 'users/:username',
+    :controller => 'users',
+    :action => 'show'
 
-  #map.codesbycategory 'codes/category/:name', :controller => 'codes', :action => 'category'
-  #map.codesbylanguagename 'codes/language/:name', :controller => 'codes', :action => 'language'
+  map.signup 'signup', 
+    :controller => 'users', 
+    :action => 'new'
   
-  map.myprofile 'profile', :controller => 'profiles', :action => 'show'
+  map.logout 'logout', 
+    :controller => 'sessions', 
+    :action => 'destroy'
   
-  #6.4.2010
-  map.userji 'users/:username', :controller => 'users', :action => 'show'
+  map.login 'login', 
+    :controller => 'sessions', 
+    :action => 'new'
   
-   #dodano za edit 8.4.2010
- # map.profile 'profile/:id', :controller => 'profiles', :action => 'edit'
-  
-  
- # map.users.connect ':controller/:action/:username'
-
-  map.signup 'signup', :controller => 'users', :action => 'new'
-  map.logout 'logout', :controller => 'sessions', :action => 'destroy'
-  map.login 'login', :controller => 'sessions', :action => 'new'
+  map.help 'help',
+    :controller => 'help',
+    :action => 'index'
   
   map.resources :sessions
 
@@ -59,47 +61,4 @@ ActionController::Routing::Routes.draw do |map|
   map.connect "*anything",
     :controller => "main",
     :action => "error404"
-
-
-  # The priority is based upon order of creation: first created -> highest priority.
-
-  # Sample of regular route:
-  #   map.connect 'products/:id', :controller => 'catalog', :action => 'view'
-  # Keep in mind you can assign values other than :controller and :action
-
-  # Sample of named route:
-  #   map.purchase 'products/:id/purchase', :controller => 'catalog', :action => 'purchase'
-  # This route can be invoked with purchase_url(:id => product.id)
-
-  # Sample resource route (maps HTTP verbs to controller actions automatically):
-  #   map.resources :products
-
-  # Sample resource route with options:
-  #   map.resources :products, :member => { :short => :get, :toggle => :post }, :collection => { :sold => :get }
-
-  # Sample resource route with sub-resources:
-  #   map.resources :products, :has_many => [ :comments, :sales ], :has_one => :seller
-  
-  # Sample resource route with more complex sub-resources
-  #   map.resources :products do |products|
-  #     products.resources :comments
-  #     products.resources :sales, :collection => { :recent => :get }
-  #   end
-
-  # Sample resource route within a namespace:
-  #   map.namespace :admin do |admin|
-  #     # Directs /admin/products/* to Admin::ProductsController (app/controllers/admin/products_controller.rb)
-  #     admin.resources :products
-  #   end
-
-  # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
-  # map.root :controller => "welcome"
-
-  # See how all your routes lay out with "rake routes"
-
-  # Install the default routes as the lowest priority.
-  # Note: These default routes make all actions in every controller accessible via GET requests. You should
-  # consider removing or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
 end
