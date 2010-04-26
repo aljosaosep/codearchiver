19e3e6f22128005e3b23dc9c5929cc9990fe842a
aljosa.osep <aljosa.osep@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-04-26 21:06:08 +0000

    Podobno kot prej uredil admin view za userje in ocene

--- trunk/CodeArchiver/db/schema.rb
+++ trunk/CodeArchiver/db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100419131134) do
+ActiveRecord::Schema.define(:version => 20100422182613) do
 
   create_table "categories", :force => true do |t|
     t.string   "name"
@@ -66,6 +66,9 @@ ActiveRecord::Schema.define(:version => 20100419131134) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "listing"
+    t.string   "filename"
+    t.string   "thumbnail"
+    t.integer  "size"
   end
 
   create_table "program_languages", :force => true do |t|
