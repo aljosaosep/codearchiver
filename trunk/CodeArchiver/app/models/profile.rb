e807c16f89510a89330d427fa706bc3a118cb416
aljosa.osep <aljosa.osep@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-04-26 19:09:14 +0000

    Dodan image-upload sistem in knjiznjica attachment_fu. Potrebno je pognati migrejt.

--- trunk/CodeArchiver/app/models/profile.rb
+++ trunk/CodeArchiver/app/models/profile.rb
@@ -1,3 +1,14 @@
 class Profile < ActiveRecord::Base
+  has_attachment :content_type => :image,
+                 :storage => :file_system,
+                 :max_size => 100.kilobytes,
+                 :resize_to => '100x100>',
+                 :thumbnails => { :thumb => '50x50>' },
+                 :processor => 'ImageScience'
+
+	#validates_as_attachment
+
   belongs_to :user
+
+
 end
