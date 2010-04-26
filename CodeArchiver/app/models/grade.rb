cbb2bdfb7cecf833a0b0712d04712a01d727449b
aljosa.osep <aljosa.osep@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-04-26 20:36:14 +0000

    Dodanih nekaj model restrikcij, default slika in metoda, ki v primeru ne-uploadane slike vrne default image path.

--- CodeArchiver/app/models/grade.rb
+++ CodeArchiver/app/models/grade.rb
@@ -1,6 +1,16 @@
+# Last edit by Aljosa Apr 26 10
+
 class Grade < ActiveRecord::Base
+  # relationships
   belongs_to :user
   belongs_to :code
+
+  # presence validation
   validates_presence_of :user_id
   validates_presence_of :code_id
+  validates_presence_of :value	
+  #validates_presence_of :id
+
+  # uniquness validation
+  validates_uniqueness_of :id
 end
--- CodeArchiver/app/models/type.rb
+++ CodeArchiver/app/models/type.rb
@@ -1,2 +1,12 @@
+# Last edit by Aljosa Apr 26 10
+
 class Type < ActiveRecord::Base
+	# relationships
+	belongs_to :code
+
+	# presence validation
+	validates_presence_of :name
+
+	# uniquness validation
+	validates_uniqueness_of :id
 end
