24dd4d8c0f43add5ff2f6eb61f5d2af3bdc286bc
LaMM3R <LaMM3R@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-04-27 11:28:47 +0000

    urejene omejitve za modele : categories, subcategories, comments, codereplies

--- CodeArchiver/app/models/code_reply.rb
+++ CodeArchiver/app/models/code_reply.rb
@@ -1,3 +1,5 @@
 class CodeReply < ActiveRecord::Base
   belongs_to :code
+  validates_presence_of :child_id
+  validates_presence_of :code_id
 end
--- CodeArchiver/app/models/subcategory.rb
+++ CodeArchiver/app/models/subcategory.rb
@@ -1,3 +1,5 @@
 class Subcategory < ActiveRecord::Base
   belongs_to :category
+  validates_presence_of :child_id
+  validates_presence_of :category_id
 end
