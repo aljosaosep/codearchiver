3416ea65a40e2e746a4b1be6f64a445102d06d29
jernej.halozan@gmail.com <jernej.halozan@gmail.com@90ac716d-75b0-d8db-48d9-f57f9a13a0bd>
2010-05-21 10:21:54 +0000

    posodobitev prve strani

--- trunk/CodeArchiver/app/controllers/main_controller.rb
+++ trunk/CodeArchiver/app/controllers/main_controller.rb
@@ -1,3 +1,9 @@
 class MainController < ApplicationController
   layout :default.to_s
+  
+  def index
+    @top_5 = Code.find(:all, :order => "popularity DESC", :limit => 5)
+    @top_5_comments = Comment.find(:all, :order => "updated_at DESC", :limit => 5)
+  end
+  
 end
